37c92afb400379224c37cded2d2ec51474f212f2
HP - Pawel Zawada <zawadapawelmail@gmail.com>
2016-03-15 18:48:12 +0100

    Initial Commit

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<html lang="NL">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="icon" href="img/favicon.ico">
+	<link rel="stylesheet" href="css/stylesheet.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100' rel='stylesheet' type='text/css'>
+	<title>Pizza Soprano</title>	
+</head>
+<body>
+<!--== start body ==-->
+
+<!-- start navbar div -->
+<div class="navbar">
+<ul>
+	<li><a href="index.php">Home</a></li>
+	<li><a href="bestellen.php">Bestellen</a></li>
+	<li><a href="contact.php">Contact</a></li>
+</ul>
+</div>
+<!-- end navbar div -->
+
+<!-- start content div -->
+<div class="content">
+	<p>This is a testing text</p>
+</div>
+<!-- end content div -->
+
+<!--== end body ==-->
+<script src="js/jquery.js"></script>
+<script src="js/application.js"></script>
+</body>
+</html>
